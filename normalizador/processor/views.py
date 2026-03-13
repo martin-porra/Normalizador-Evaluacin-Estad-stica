89098754fb865b03e55a56020b2863a2fdfb54f2
Martin Porra <martinpp12@gmail.com>
2026-03-13 14:03:33 -0300

    agrega servicios cleaner, template upload y cambios en views

--- normalizador/processor/views.py
+++ normalizador/processor/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
 
 def upload(request):
-    return HttpResponse("Pagina de subida de archivos")
+
+    if request.method == "POST":
+
+        archivo = request.FILES["archivo"]
+
+        if archivo.name.endswith(".csv"):
+            df = pd.read_csv(archivo)
+
+        elif archivo.name.endswith(".xlsx"):
+            df = pd.read_excel(archivo)
+
+        else:
+            return render(request, "upload.html", {"error": "Formato no soportado"})
+
+        tabla = df.head().to_html()
+
+        return render(request, "upload.html", {"tabla": tabla})
+
+    return render(request, "upload.html")
